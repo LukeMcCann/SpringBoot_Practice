2a601754002220cc2408432f9a748096dbee596a
Luke McCann <developer-lukemccann@outlook.com>
2019-11-27 20:16:46 +0000

    Adds notes on RequestMapping

--- src/main/java/com/planetdebug/firstMavenProject/hello/HelloController.java
+++ src/main/java/com/planetdebug/firstMavenProject/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.planetdebug.firstMavenProject.hello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,4 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController
 {
+
+    /**
+     * RequestMapping - maps request of of the url to the method (/hello is the url which is mapped to this method)
+     * This annotation tells the spring framework that whenever there is a HTTP request to this application at the url "/hello"
+     * we want this method to be executed.
+     *
+     * RequestMapping maps only map to GET by default, to map to other HTTP methods you have to specify it in the annotation.
+     *
+     * @return String
+     */
+    @RequestMapping("/hello")
+    public String helloWorld()
+    {
+        return("Hello World!");
+    }
 }
